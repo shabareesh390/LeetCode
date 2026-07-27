d3112eefada16c8322c3e9157995794f431e47bc
Shabareesh S Gowda <shabareesh390@gmail.com>
2026-07-27 13:48:35 +0530

    Time: 2 ms (96.44%), Space: 45.7 MB (51.7%) - LeetHub

---
+++ 0039-combination-sum/0039-combination-sum.java
@@ -0,0 +1,26 @@
+class Solution {
+    public List<List<Integer>> combinationSum(int[] candidates, int target) {
+        List<List<Integer>> ans = new ArrayList<>();
+        backtrack(candidates, target, 0, new ArrayList<>(), ans);
+        return ans;
+    }
+
+    private void backtrack(int[] candidates, int target, int index,
+                           List<Integer> current, List<List<Integer>> ans) {
+
+        if (target == 0) {
+            ans.add(new ArrayList<>(current));
+            return;
+        }
+
+        if (target < 0) {
+            return;
+        }
+
+        for (int i = index; i < candidates.length; i++) {
+            current.add(candidates[i]);
+            backtrack(candidates, target - candidates[i], i, current, ans);
+            current.remove(current.size() - 1);
+        }
+    }
+}
