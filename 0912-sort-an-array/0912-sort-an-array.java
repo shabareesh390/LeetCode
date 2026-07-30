61a34d0161963ecbb8e08032cd539c7ebcfacf6d
Shabareesh S Gowda <shabareesh390@gmail.com>
2026-07-30 10:29:15 +0530

    Time: 31 ms (28.89%), Space: 74.6 MB (98.31%) - LeetHub

--- 0912-sort-an-array/0912-sort-an-array.java
+++ 0912-sort-an-array/0912-sort-an-array.java
@@ -1,43 +1,46 @@
 class Solution {
     public int[] sortArray(int[] nums) {
-        mergeSort(nums, 0, nums.length - 1);
-        return nums;
+        
+        return sort(nums);
     }
 
-    void mergeSort(int[] arr, int left, int right) {
-        if (left >= right) return;
+    static int[] sort(int[] arr){
+        if(arr.length==1){
+            return arr;
+        }
+        int mid= arr.length/2;
+        int[] left=sort(Arrays.copyOfRange(arr,0,mid));
+        int[] right=sort(Arrays.copyOfRange(arr,mid,arr.length));
+        return merge(left,right);
 
-        int mid = left + (right - left) / 2;
-
-        mergeSort(arr, left, mid);
-        mergeSort(arr, mid + 1, right);
-
-        merge(arr, left, mid, right);
     }
+    static int[] merge(int[] first,int[] second){
+         int[] mix=new int[first.length+ second.length];
+         int i=0;
+         int j=0;
+         int k=0;
 
-    void merge(int[] arr, int left, int mid, int right) {
-        int[] temp = new int[right - left + 1];
-
-        int i = left, j = mid + 1, k = 0;
-
-        while (i <= mid && j <= right) {
-            if (arr[i] <= arr[j]) {
-                temp[k++] = arr[i++];
-            } else {
-                temp[k++] = arr[j++];
-            }
-        }
-
-        while (i <= mid) {
-            temp[k++] = arr[i++];
-        }
-
-        while (j <= right) {
-            temp[k++] = arr[j++];
-        }
-
-        for (int x = 0; x < temp.length; x++) {
-            arr[left + x] = temp[x];
-        }
+         while(i< first.length && j< second.length){
+             if(first[i]<second[j]){
+                 mix[k]=first[i];
+                 i++;
+             }
+             else{
+                 mix[k]=second[j];
+                 j++;
+             }
+             k++;
+         }
+         while(i< first.length){
+             mix[k]=first[i];
+             i++;
+             k++;
+         }
+         while(j< second.length){
+             mix[k]=second[j];
+             j++;
+             k++;
+         }
+        return mix;
     }
 }
