3a6e2a25bcddf68956ce0aca2075fe06f93f6cea
Shabareesh S Gowda <shabareesh390@gmail.com>
2026-08-01 10:54:12 +0530

    Time: 276 ms (9.46%), Space: 88 MB (5.2%) - LeetHub

--- 0912-sort-an-array/0912-sort-an-array.java
+++ 0912-sort-an-array/0912-sort-an-array.java
@@ -1,46 +1,34 @@
 class Solution {
     public int[] sortArray(int[] nums) {
-        
-        return sort(nums);
+        sort(nums,0,nums.length-1);
+        return nums ;
     }
 
-    static int[] sort(int[] arr){
-        if(arr.length==1){
-            return arr;
+    static void sort(int[] arr,int low,int hi){
+        if(low>=hi){
+         return;
         }
-        int mid= arr.length/2;
-        int[] left=sort(Arrays.copyOfRange(arr,0,mid));
-        int[] right=sort(Arrays.copyOfRange(arr,mid,arr.length));
-        return merge(left,right);
+        int s=low;
+        int e=hi;
+        int mid=s+(e-s)/2;
+        int pivot=arr[mid];
+        while(s<=e){
+            while (arr[s]<pivot){
+                s++;
+            }
+            while (arr[e]>pivot){
+                e--;
+            }
+            if(s<=e){
+                int temp=arr[s];
+                arr[s]=arr[e];
+                arr[e]=temp;
+                s++;
+                e--;
+            }
+        }
+        sort(arr,low, e);
+        sort(arr,s,hi);
 
-    }
-    static int[] merge(int[] first,int[] second){
-         int[] mix=new int[first.length+ second.length];
-         int i=0;
-         int j=0;
-         int k=0;
-
-         while(i< first.length && j< second.length){
-             if(first[i]<second[j]){
-                 mix[k]=first[i];
-                 i++;
-             }
-             else{
-                 mix[k]=second[j];
-                 j++;
-             }
-             k++;
-         }
-         while(i< first.length){
-             mix[k]=first[i];
-             i++;
-             k++;
-         }
-         while(j< second.length){
-             mix[k]=second[j];
-             j++;
-             k++;
-         }
-        return mix;
     }
 }
