80b95dee5d97ae42810e678022ab9c5412696757
Shabareesh S Gowda <shabareesh390@gmail.com>
2026-08-05 19:21:53 +0530

    Time: 1 ms (71.89%), Space: 44.7 MB (14.09%) - LeetHub

---
+++ 2011-final-value-of-variable-after-performing-operations/2011-final-value-of-variable-after-performing-operations.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int finalValueAfterOperations(String[] operations) {
+        int p=0;
+        for(int i=0;i<operations.length;i++){
+           if( operations[i].equals("--X") || operations[i].equals("X--")){
+            p--;
+           }
+          else p++;
+        }
+        return p;
+    }
+}
