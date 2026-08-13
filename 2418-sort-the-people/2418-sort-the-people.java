1c6e275be5a72231aa28ef6c1e59da6dc885030a
Shabareesh S Gowda <shabareesh390@gmail.com>
2026-08-13 10:01:05 +0530

    Time: 21 ms (22.07%), Space: 47.3 MB (55.21%) - LeetHub

--- 2418-sort-the-people/2418-sort-the-people.java
+++ 2418-sort-the-people/2418-sort-the-people.java
@@ -3,17 +3,24 @@ class Solution {
         for(int i=0;i<heights.length;i++){
             for(int j=i+1;j<heights.length;j++){
                 if(heights[i]<heights[j]){
-                    String temp=names[j];
-                    names[j]=names[i];
-                    names[i]=temp;
-
-                    int tempHeight = heights[i];
-                    heights[i] = heights[j];
-                    heights[j] = tempHeight;
+                    swap(heights,i,j);
+                    swap(names,i,j);
                 }
             }
         }
         return names;
     }
+     static void swap(int[] heights, int i, int j) {
+        int temp = heights[i];
+        heights[i] = heights[j];
+        heights[j] = temp;
+    }
+
+    static void swap(String[] names, int i, int j) {
+        String temp = names[i];
+        names[i] = names[j];
+        names[j] = temp;
+    }
+   
     
 }
