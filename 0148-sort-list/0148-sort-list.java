ed1dc57ff4b46b687f89a986b819aaf90dfc5bc8
Shabareesh S Gowda <shabareesh390@gmail.com>
2026-08-17 21:21:11 +0530

    Time: 14 ms (23.53%), Space: 65.1 MB (6.57%) - LeetHub

---
+++ 0148-sort-list/0148-sort-list.java
@@ -0,0 +1,50 @@
+class Solution {
+    public ListNode sortList(ListNode head) {
+        if (head == null || head.next == null) {
+            return head;
+        }
+
+        ListNode mid = getMid(head);
+
+        
+        ListNode rightHead = mid.next;
+        mid.next = null;
+
+        
+        ListNode left = sortList(head);
+        ListNode right = sortList(rightHead);
+
+        
+        return merge(left, right);
+    }
+
+    static ListNode getMid(ListNode head) {
+        ListNode slow = head;
+        ListNode fast = head.next;
+
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+
+        return slow;
+    }
+
+    static ListNode merge(ListNode list1, ListNode list2) {
+        if (list1 == null) {
+            return list2;
+        }
+
+        if (list2 == null) {
+            return list1;
+        }
+
+        if (list1.val <= list2.val) {
+            list1.next = merge(list1.next, list2);
+            return list1;
+        } else {
+            list2.next = merge(list1, list2.next);
+            return list2;
+        }
+    }
+}
