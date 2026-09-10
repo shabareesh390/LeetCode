ed7c3d9c7e2bf98d256a4b7c505142719d3b1947
Shabareesh S Gowda <shabareesh390@gmail.com>
2026-09-10 20:30:36 +0530

    Time: 0 ms (100%), Space: 43.4 MB (85.23%) - LeetHub

--- 0101-symmetric-tree/0101-symmetric-tree.java
+++ 0101-symmetric-tree/0101-symmetric-tree.java
@@ -18,27 +18,20 @@ class Solution {
         if (root == null) {
             return true;
         }
-
-        return check(root.left, root.right);
+        return check(root.left,root.right);
     }
-
-    public boolean check(TreeNode left, TreeNode right) {
-        
-        if (left == null && right == null) {
+    static boolean check(TreeNode left,TreeNode right){
+        if(left==null && right==null){
             return true;
         }
-
-        if (left == null || right == null) {
+        if(left==null || right==null){
             return false;
         }
-
-        
-        if (left.val != right.val) {
+        if(left.val != right.val){
             return false;
         }
-
-       
-        return check(left.left, right.right) &&
-               check(left.right, right.left);
+        return check(left.left,right.right) && check(left.right,right.left);
     }
+
+    
 }
