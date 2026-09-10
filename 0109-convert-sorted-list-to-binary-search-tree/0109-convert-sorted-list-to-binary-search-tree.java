faa0d0ba121ceccaa8e805ffacd2c48ec5bd182a
Shabareesh S Gowda <shabareesh390@gmail.com>
2026-09-10 20:12:34 +0530

    Time: 0 ms (100%), Space: 46.8 MB (58.25%) - LeetHub

--- 0109-convert-sorted-list-to-binary-search-tree/0109-convert-sorted-list-to-binary-search-tree.java
+++ 0109-convert-sorted-list-to-binary-search-tree/0109-convert-sorted-list-to-binary-search-tree.java
@@ -25,23 +25,23 @@
  */
 class Solution {
     public TreeNode sortedListToBST(ListNode head) {
-        if(head == null){
-            return null;
-        }
-        if(head.next == null){
-            return new TreeNode(head.val);
-        }
-        ListNode mid=findMid(head);
-        TreeNode root=new TreeNode(mid.val);
-        root.left=sortedListToBST(head);
-        root.right=sortedListToBST(mid.next);
-        return root;
+       if(head == null){
+        return null;
+       }
+       if(head.next == null){
+        return new TreeNode(head.val);
+       }
+       ListNode mid=findMid(head);
+       TreeNode root=new TreeNode(mid.val);
+       root.left=sortedListToBST(head);
+       root.right=sortedListToBST(mid.next);
+       return root;
 
     }
     static ListNode findMid(ListNode head){
-        ListNode prev=null;
         ListNode slow=head;
         ListNode fast=head;
+        ListNode prev=null;
         while(fast != null && fast.next != null){
             prev=slow;
             slow=slow.next;
