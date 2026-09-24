04f6aa4638140b00ae467830bc1a7732acd5122f
Shabareesh S Gowda <shabareesh390@gmail.com>
2026-09-24 20:48:18 +0530

    Time: 0 ms (100%), Space: 43 MB (87.18%) - LeetHub

--- 0993-cousins-in-binary-tree/0993-cousins-in-binary-tree.java
+++ 0993-cousins-in-binary-tree/0993-cousins-in-binary-tree.java
@@ -20,7 +20,10 @@ class Solution {
         }
         TreeNode xx=findNode(root,x);
         TreeNode yy=findNode(root,y);
-        return level(root,xx,0)==level(root,yy,0) && !isSib(root,xx,yy);
+
+        return (
+            (level(root,xx,0)==level(root,yy,0)) && !isSib(root,xx,yy)
+        );
 
     }
     static TreeNode findNode(TreeNode root,int x){
@@ -30,24 +33,18 @@ class Solution {
         if(root.val == x){
             return root;
         }
-        TreeNode l=findNode(root.left,x);
-        if(l != null){
-            return l;
+        TreeNode n=findNode(root.left,x);
+        if(n != null){
+            return n;
         }
         return findNode(root.right,x);
-    }
-    static boolean isSib(TreeNode root,TreeNode x,TreeNode y){
-        if(root == null){
-            return false;
-        }
-        return ((root.left==x && root.right==y) || (root.left==y && root.right==x) || (isSib(root.left,x,y)) || (isSib(root.right,x,y)));
     }
     static int level(TreeNode root,TreeNode x,int lev){
         if(root == null){
             return 0;
         }
-        if(root == x){
-            return lev; 
+        if(root==x){
+            return lev;
         }
         int l=level(root.left,x,lev+1);
         if(l != 0){
@@ -55,4 +52,12 @@ class Solution {
         }
         return level(root.right,x,lev+1);
     }
+    static boolean isSib(TreeNode root,TreeNode x,TreeNode y){
+        if(root == null){
+            return false;
+        }
+        return (root.left==x && root.right==y) || (root.left==y && root.right==x) || isSib(root.left,x,y) || isSib(root.right,x,y);
+    }
+   
+    
 }
